605b6f5633d8f161b2ad9026efdc3c9ec33fa51f
Mateusz Kamola <mateusz.kamola@gmail.com>
2023-05-21 23:48:59 +0200

    Implementing serializing new data added over http

--- src/bin/httptodo2.rs
+++ src/bin/httptodo2.rs
@@ -1,16 +1,24 @@
 //use std::io::{Error,BufRead};
 use serde::{Serialize,Deserialize};
+use rocket::serde::json::Json;
 use rocket::State;
+use std::sync::{atomic::{AtomicBool,AtomicUsize,Ordering},Mutex};
 #[macro_use] extern crate rocket;
 
 #[derive(Debug,Serialize,Deserialize)]
 struct TodoList {
     items: Vec<TodoItem>,
-    current_id: usize,
-    dirty: bool
+    current_id: usize
+}
+
+struct TodoListState {
+    items: Mutex<Vec<TodoItem>>,
+    current_id: AtomicUsize,
+    filestore: String
 }
 
 #[derive(Debug,Serialize,Deserialize)]
+#[serde(crate = "rocket::serde")]
 struct TodoItem {
     id: usize,
     content: String
@@ -18,9 +26,15 @@ struct TodoItem {
 
 #[launch]
 fn rocket() -> _ {
-    let todo_list = load_todo_list();
+    let args: Vec<String> = std::env::args().collect();
+    let todo_list = load_todo_list(&args[1]);
+    let todo_list_state = TodoListState {
+        items: Mutex::new(todo_list.items),
+        current_id: AtomicUsize::new(todo_list.current_id),
+        filestore: args[1]
+    };
     rocket::build()
-        .manage(todo_list)
+        .manage(todo_list_state)
         .mount("/", routes![list])
 }
 
@@ -29,14 +43,41 @@ fn list(todo_list_state: &State<TodoList>) -> String {
     serde_json::to_string::<TodoList>(todo_list_state).unwrap()
 }
 
-fn load_todo_list() -> TodoList {
-    let default_todo_list = TodoList{
-        items: Vec::new(),
-        current_id: 1,
-        dirty: false
+#[post("/notes", format = "json", data="<todo_item>")]
+fn create(todo_item: Json<TodoItem>, todo_list_state: &State<TodoListState>) -> String {
+    let curr_id = todo_list_state.current_id.fetch_add(1, Ordering::SeqCst);
+    let new_item = TodoItem {
+        id: curr_id,
+        content: todo_item.content.clone()
     };
-    let args: Vec<String> = std::env::args().collect();
-    let file = std::fs::File::open(&args[1]);
+    let mut items = todo_list_state.items.lock().unwrap();
+    items.push(new_item);
+    write_todo_list(todo_list_state.filestore, items, todo_list_state.load(Ordering::SeqCst));
+    format!("Added {}", todo_item.content)
+
+}
+
+fn write_todo_list(filestore: String, items: Vec<TodoItem>, id: usize) {
+    let todo_list = TodoList {
+        items,
+        current_id: id
+    };
+    match std::fs::File::create(&filestore) {
+        Ok(f) => match serde_json::to_writer(f, &todo_list) {
+            Ok(_) => println!("Serialized successfuly"),
+            Err(x) => println!("Error when serializing {}", x)
+        },
+        Err(x) => println!("Error when serializing {}", x)
+    };
+    
+}
+
+fn load_todo_list(filestore: &String) -> TodoList {
+    let default_todo_list = TodoList {
+        items: Vec::new(),
+        current_id: 1
+    };
+    let file = std::fs::File::open(filestore);
     let todo_list = match file {
         Ok(f) => match serde_json::from_reader(f) {
             Ok(data) => data,
